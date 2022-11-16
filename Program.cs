f540a119ad21ab8f46b3bcb79bab046654f34879
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-11-15 22:54:04 -0300

    Create a loop that ask for each score and sum them all

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿Console.WriteLine("Digite a quantidade de notas a ser calculada:");
 int quantidadeNotas = Convert.ToInt32(Console.ReadLine());
+
+double somaDasNotas = 0;
+for (int contador = 1; contador <= quantidadeNotas; contador++)
+{
+    Console.WriteLine($"Digite a {contador}ª nota:");
+    double nota = Convert.ToDouble(Console.ReadLine());
+    somaDasNotas += nota;
+}
